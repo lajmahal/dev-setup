c6286783c2056507220b3319823c97cfebcf5dae
Laj Lal <laj.lal@outlook.com>
2017-04-06 07:40:05 +0800

    Adding bash files and setting up symlinks

--- make_my_dev_box.sh
+++ make_my_dev_box.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -x
+#set -x
 
-# 1. Install vim 
+echo "Installing vim" 
 sudo apt-get install vim 
+
+echo "Setting up symlinks"
+cd ~ 
+rm -rf .profile && ln -snf ~/dev/dev-setup/profile .profile 
+rm -rf .bashrc && ln -snf ~/dev/dev-setup/bashrc .bashrc 
+
