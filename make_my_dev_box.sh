55bab1966bbf4bdbe76d5cb99edb7f7d340e082f
Laj Lal <laj.lal@outlook.com>
2017-04-06 07:48:20 +0800

    Install tmux and add additional symlinks

--- make_my_dev_box.sh
+++ make_my_dev_box.sh
@@ -5,8 +5,13 @@
 echo "Installing vim" 
 sudo apt-get install vim 
 
+echo "Installing tmux" 
+sudo apt-get install tmux
+
 echo "Setting up symlinks"
 cd ~ 
 rm -rf .profile && ln -snf ~/dev/dev-setup/profile .profile 
 rm -rf .bashrc && ln -snf ~/dev/dev-setup/bashrc .bashrc 
+rm -rf .bash_aliases && ln -snf ~/dev/dev-setup/bash_aliases .bash_aliases 
+rm -rf .tmux.conf && ln -snf ~/dev/dev-setup/tmux.conf .tmux.conf
 
