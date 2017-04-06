02d68a0e9af6fdc56174965c8a570b8a27cf11be
Laj Lal <laj.lal@outlook.com>
2017-04-06 21:12:27 +0800

    Adding powerline installation

--- make_my_dev_box.sh
+++ make_my_dev_box.sh
@@ -8,6 +8,18 @@ sudo apt-get install vim
 echo "Installing tmux" 
 sudo apt-get install tmux
 
+echo "Installing pip" 
+sudo apt-get install python-pip
+pip install --upgrade pip
+
+# Powerline - http://askubuntu.com/questions/283908/how-can-i-install-and-use-powerline-plugin
+echo "Installing powerline" 
+pip install --user git+git://github.com/Lokaltog/powerline
+wget 'https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf' 'https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf'
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+
 echo "Setting up symlinks"
 cd ~ 
 rm -rf .profile && ln -snf ~/dev/dev-setup/profile .profile 
